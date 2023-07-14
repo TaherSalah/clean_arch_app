8869f874c4b4d982ca30c3018490cf5754743280
TaherSalah <tahersalah2016@gmail.com>
2023-07-14 18:33:44 +0300

    frst commit

---
+++ android/app/src/main/kotlin/com/example/clean_artch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clean_artch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constances.dart
@@ -0,0 +1,4 @@
+class AppConstance {
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const apiKey = '3fcf81928648ed81141be245e032222c';
+}
